83c86f2b9945d576edf38d3313f9c99d192f3742
Soumitra Varindra Chakravarti <svc@Soumitras-MacBook-Air.local>
2019-09-24 15:22:45 +0530

    Stable Release

--- notes.js
+++ notes.js
@@ -5,39 +5,77 @@ const fs = require("fs");
 // Loading notes
 const loadNotes = () => {
     try {
-        const notes = fs.readFileSync("./notes.json").toString();
-        return notes;
-    } catch (error) {
-        console.log(chalk.red.inverse("Notes not found!"));
-        return [];
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
     }
 }
 
 // Savings notes
 const saveNotes = (notes) => {
-    fs.writeFileSync("./notes.json", JSON.parse(notes));
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
-// Adding a note
+// Adding a note.
 const addNote = (title, body) => {
     const notes = loadNotes();
-    const newNote = notes.find((note) => {
-        if (note.title === title) {
-            console.log(chalk.red.inverse("Note already exists!"));
-        } else {
-            notes.push(newNote);
-            saveNotes(notes);
-            console.log(chalk.green.inverse("Note added!"));
-        }
-    });
+    const duplicateNote = notes.find((note) => note.title === title);
+
+// Checking for duplicate entry
+    if (duplicateNote) {
+        console.log(chalk.red.inverse("Note exists!"));
+        
+    } else {
+        notes.push({
+            title: title,
+            body: body
+        });
+        saveNotes(notes);
+        console.log(chalk.green.inverse("Notes added!"));
+        
+    }
 }
 
 // Removing a note
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
 // Listing a note
+const listNotes = () => {
+    const notes = loadNotes()
 
+    console.log(chalk.inverse('Notes'))
+
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
 // Reading a note
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
 
+    if (note) {
+        console.log(chalk.green.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
 module.exports = {
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
 }
